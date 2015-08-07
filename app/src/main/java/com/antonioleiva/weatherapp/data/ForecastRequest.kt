1688724f6d470e94ae49e5c6674456f5b60b96a4
Antonio Leiva <antonioleivag@gmail.com>
2015-08-07 08:46:12 +0200

    Chapter 14: Visibility modifiers

--- app/src/main/java/com/antonioleiva/weatherapp/data/ForecastRequest.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/data/ForecastRequest.kt
@@ -3,7 +3,7 @@ package com.antonioleiva.weatherapp.data
 import com.google.gson.Gson
 import java.net.URL
 
-public class ForecastRequest(val zipCode: String) {
+class ForecastRequest(val zipCode: String) {
 
     companion object {
         private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
@@ -11,7 +11,7 @@ public class ForecastRequest(val zipCode: String) {
         private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
     }
 
-    public fun execute(): ForecastResult {
+    fun execute(): ForecastResult {
         val forecastJsonStr = URL(COMPLETE_URL + zipCode).readText()
         return Gson().fromJson(forecastJsonStr, ForecastResult::class.java)
     }
